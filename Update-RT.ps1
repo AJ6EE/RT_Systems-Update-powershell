f5c976b602f6282e1dd2755cbef79d11a25e6963
AJ6EE <63912806+AJ6EE@users.noreply.github.com>
2026-02-02 08:23:52 -0800

    Add files via upload

---
+++ Update-RT.ps1
@@ -0,0 +1,8 @@
+﻿$basePath = "C:\RT Systems V5"
+
+Get-ChildItem -Path $basePath -Recurse -Filter RTUpdater_V5.exe | ForEach-Object {
+    Write-Host "Launching updater: $($_.FullName)"
+    Start-Process -FilePath $_.FullName
+    Start-Sleep -Seconds 8
+}
+
